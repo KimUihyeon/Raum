d3ceadd518e366b623a0e97198bae4d90511c9a3
kuh for mac <core.quotepath>
2020-01-02 03:43:43 +0900

    first commit

---
+++ source/src/main/java/com/study/raum/RaumApplication.java
@@ -0,0 +1,13 @@
+package com.study.raum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaumApplication.class, args);
+    }
+
+}
---
+++ source/src/test/java/com/study/raum/RaumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.raum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
