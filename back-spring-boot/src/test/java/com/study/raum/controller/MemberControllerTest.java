60dbbd264a73d274615caafc346d870003c96a41
kuh for mac <dkrnl1318@naver.com>
2020-03-02 12:04:27 +0900

    feat:Object Test Commit

--- back-spring-boot/src/test/java/com/study/raum/controller/MemberControllerTest.java
+++ back-spring-boot/src/test/java/com/study/raum/controller/MemberControllerTest.java
@@ -1,6 +1,9 @@
 package com.study.raum.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.raum.model.Member;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = MemberController.class)
@@ -25,11 +29,45 @@ public class MemberControllerTest {
 
     @Test
     @Rollback
-    public void TestMethod() throws Exception{
+    public void MvcTest() throws Exception{
+
+        Member member = new Member();
+        member.setName("test");
+        member.setId(3);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String memberSerializeStr = mapper.writeValueAsString(member);
 
         mock.perform(get("/api/member/3"))
                 .andDo(print())
-                .andExpect(content().string(("{\"id\":3,\"name\":\"test\",\"tel\":null,\"email\":null}")))
+                .andExpect(content().string(memberSerializeStr))
                 .andExpect(jsonPath("$.id").value(3));
+    }
+
+    @Test
+    public void MvcJsonTest() throws Exception {
+        int memberID = 2;
+
+        Member member = Member.builder()
+                                .id(memberID)
+                                .name("김의현")
+                                .build();
+
+
+        mock.perform(get("/api/member/" + memberID))
+                .andDo(print())
+                .andExpect(jsonPath("$.id").value(memberID));
+    }
+
+    @Test
+    public void objectTest() throws Exception {
+
+        Member member = new Member();
+        member.setName("test");
+        String data = String.valueOf(member.getName());
+
+
+
+        assertThat(member).isNotNull();
     }
 }
