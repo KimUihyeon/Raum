6a2d2a3b39fca1e44210eaa10ee5aef3edbc7b85
kuh for mac <dkrnl1318@naver.com>
2020-03-17 17:17:19 +0900

    feat:Axios Wrapper Class 비동기 통신 공통클래스 만듬.. 계속 업그레이드 하자

---
+++ front-react/src/util/AxiosUtile.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+
+// 작성 규칙 - 마지막 슬래시 생략 할것 ..!
+const dev_server : String = "http://localhost:8080";
+const rel_server : String = "";
+
+
+
+// real Connection String;
+const server_connection_str = dev_server;
+
+
+
+/**
+ * 2020 . 03. 17 업그레이드 중..!!
+ */
+class AxiosUtil {
+
+    private getUrl(url : string){
+        return [server_connection_str, url].join('');
+    }
+
+    data(url : string){
+        return axios.get(this.getUrl(url));
+    }
+
+    patch(url : string , data : any){
+        return axios.patch(this.getUrl(url), {...data});
+    }
+
+    post(url : string , data : any){
+        return axios.post(this.getUrl(url), {...data});
+    }
+
+    delete(url : string){
+        return axios.delete(this.getUrl(url));
+    }
+
+}
+
+export const axiosUtil = new AxiosUtil();
