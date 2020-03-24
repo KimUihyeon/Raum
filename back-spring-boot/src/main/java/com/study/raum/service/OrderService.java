f22208765179ef76dc9f89f729e61f431cd20cbd
kuh for mac <dkrnl1318@naver.com>
2020-03-25 01:07:07 +0900

    feat:Order Service Temp 임시저장

--- back-spring-boot/src/main/java/com/study/raum/service/OrderService.java
+++ back-spring-boot/src/main/java/com/study/raum/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.study.raum.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author kuh
  * @since 2020.03.25
  */
+@Service("orderService")
 public class OrderService {
 }
