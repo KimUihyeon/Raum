b4eb216ee340533e2785793e14de42bf29f906a0
KimUihyeon <dkrnl1318@naver.com>
2020-11-07 14:27:24 +0900

    feat:Vue FontAwesome

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/** fontawesome dependencies */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+
+/** Element Ui dependencies */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- front-vue/src/views/common/Header.vue
+++ front-vue/src/views/common/Header.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="nav">
+
         <MenuItem to='/'>Home</MenuItem> | 
         <MenuItem to='/about'>Test About</MenuItem>
     </div>
 </template>
 
 <script>
-import MenuItem from '../../components/common/MenuItem';
+import MenuItem from '../../components/common/MenuItem'
+
 const components = { MenuItem, }
 export default {
     components,
