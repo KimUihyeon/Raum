eb9dc59c26df4d6daa355f51264464ddeb7133e0
KimUihyeon <dkrnl1318@naver.com>
2020-11-07 14:15:33 +0900

    feat:vue router Init

---
+++ front-vue/src/components/common/MenuItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <span>
+      <router-link v-bind:to="to" ><slot></slot></router-link>
+    </span>
+</template>
+
+<script>  
+const props = { to : String, name : String, };
+
+export default {
+    name,
+    props,
+}
+</script>
---
+++ front-vue/src/views/common/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="nav">
+        <MenuItem to='/'>Home</MenuItem> | 
+        <MenuItem to='/about'>Test About</MenuItem>
+    </div>
+</template>
+
+<script>
+import MenuItem from '../../components/common/MenuItem';
+const components = { MenuItem, }
+export default {
+    components,
+}
+</script>
